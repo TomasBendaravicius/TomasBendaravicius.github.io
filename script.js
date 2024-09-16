c0b18e710e7a0be4681a5145352a715a2a73a0c9
TomasBendaravicius <bendaravicius1@gmail.com>
2024-09-16 19:08:44 +0300

    27

--- script.js
+++ script.js
@@ -53,10 +53,9 @@ function myFunction() {
     document.getElementById("demo").innerHTML = txt;
 }
 
-// Get the button
+
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
